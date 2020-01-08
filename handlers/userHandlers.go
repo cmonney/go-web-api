5bfcb279566701e8f2b77eda5c635658a249035a
Christopher Monney <christopher.monney@ovoenergy.com>
2020-01-08 17:49:06 +0000

    userHandlers updated

--- handlers/userHandlers.go
+++ handlers/userHandlers.go
@@ -2,8 +2,15 @@ package handlers
 
 import (
 	user "cmonney/go-web-api/users"
+	"errors"
 	"net/http"
 )
+
+func bodyToUser(r *http.Request, u *user.User) error {
+	if r.Body == nil {
+		return errors.New("Request body is empty")
+	}
+}
 
 func usersGetAll(w http.ResponseWriter, r *http.Request) {
 	users, err := user.All()
